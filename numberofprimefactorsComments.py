5a35194bf4ef47fd0b0fe4ce7075139f3d988ace
Tyler-R-Russ <164228021+Tyler-R-Russ@users.noreply.github.com>
2024-03-22 15:40:21 -0400

    Delete numberofprimefactorsComments.py
    deleting comment files

--- numberofprimefactorsComments.py
+++
@@ -1,9 +0,0 @@
-#sys.path.append('factoringprimes')
-#import numberofprimefactors
-#from numberofprimefactors import numberofprimefactors
-
-
-#Alternative:
-##def numberofprimefactors(n):
-##    lst=primefactors(n)
-##    return (len(lst))
