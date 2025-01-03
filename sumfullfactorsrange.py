96335f64e94ad8b95cb2a3ebfde2d860a4b40497
Tyler-R-Russ <164228021+Tyler-R-Russ@users.noreply.github.com>
2025-01-03 16:33:12 -0500

    Update and rename sumfullproperfactorsrange.py to sumfullfactorsrange.py
    Updated for Python version 3.
    And name changes.

--- sumfullfactorsrange.py
+++ sumfullfactorsrange.py
@@ -1,12 +1,12 @@
-import sumfullproperfactors
-from sumfullproperfactors import sumfullproperfactors
+import sumfullfactors
+from sumfullfactors import sumfullfactors
 
 #Purpose: This module calculates the sum of the factors of every number
 #in a given range a to b.
 
-def sumfullproperfactorsrange(a,b):
+def sumfullfactorsrange(a,b):
     lst=[]
     for i in range(a,b+1):
         #note to self: python indexing: range(a,b) gives (a,a+1,...,b-1)
-        lst=lst+[sumfullproperfactors(i)]
+        lst=lst+[sumfullfactors(i)]
     return(lst)
