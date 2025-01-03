d5eb53e6fe41e1b0caddd3339dbf151ed1700a28
Tyler-R-Russ <164228021+Tyler-R-Russ@users.noreply.github.com>
2025-01-03 16:30:50 -0500

    Update and rename sumfullproperfactors.py to sumfullfactors.py
    Updated for Python version 3.
    And title revisions.

--- sumfullfactors.py
+++ sumfullfactors.py
@@ -1,15 +1,9 @@
-import fullproperfactors
-from fullproperfactors import fullproperfactors
+import fullfactors
+from fullfactors import fullfactors
 
 #Purpose: This program finds the sum of the factors of a number n, including n.
 #Theory: The method is brute force and does not utilize the prime factorization of n.
 
-def sumfullproperfactors(n):
-    lst = fullproperfactors(n)
+def sumfullfactors(n):
+    lst = fullfactors(n)
     return(sum(lst))
-
-
-
-
-
-
