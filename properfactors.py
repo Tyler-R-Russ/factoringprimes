88be1a85a0a992ac8ee32b97e65c2b011f0edb43
Tyler-R-Russ <164228021+Tyler-R-Russ@users.noreply.github.com>
2025-01-03 15:58:13 -0500

    Update properfactors.py
    Updated for Python version 3.

--- properfactors.py
+++ properfactors.py
@@ -1,4 +1,4 @@
-from fractions import gcd
+from math import gcd
 from math import sqrt
 from math import ceil
 from math import floor
@@ -16,9 +16,6 @@ def properfactors(n):
      i = 2
      for i in range (2, n-1):
                     lst = range (2,n)
-                    lst = filter(lambda i:gcd (i,n) == i,lst)
+                    lst = list(filter(lambda i:gcd (i,n) == i,lst))
                     lst.insert(0,1) #Here, we include 1 as a proper factor
      return (lst)
-
-
-
