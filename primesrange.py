47eea4784ec6cbbf3086c6ecd754f0ccc67fb660
Tyler-R-Russ <164228021+Tyler-R-Russ@users.noreply.github.com>
2025-01-03 15:12:05 -0500

    Update primesrange.py
    Updated for Python version 3.

--- primesrange.py
+++ primesrange.py
@@ -7,9 +7,7 @@ def primesrange(a,b):
     if a <= 1:
         a=2
     else: a=a
-    lst = xrange(a,b+1) #Python indexing, modify the range to (a,b+1).
+    lst = range(a,b+1) #Python indexing, modify the range to (a,b+1).
     lst = [x for x in lst if len(halfproperfactors(x))==1]
     #i.e.; if 1 is the only proper factor
     return(lst)
-
-
